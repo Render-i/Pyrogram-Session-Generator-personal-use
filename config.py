eafd52ea280b1ebdfb28cabc16a0ce4519d8d60f
Render-i <pleasemegoo@outlook.com>
2024-07-15 16:22:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
+API_ID = int(getenv("API_ID", "11820035"))
+API_HASH = getenv("API_HASH", "f398cd0b3c26facb19c0f22aaeb62850")
+BOT_TOKEN = getenv("BOT_TOKEN", "7460234172:AAGQYL7JvPKUzbI0REQshm2sr06LEOMZpnI")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002172371819"))
